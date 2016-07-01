569fdcb4d0f1118c32b7ed16ebc9b7cfe521d381
vitalii.mishchenko <vitalii.mishchenko@globallogic.com>
2016-07-01 16:32:38 +0300

    change version

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,10 @@
 
 let error = require('mue-core/modules/error');
 const API_PREFIX = '/api/read-hub';
+const VERSION = '1';
 
 module.exports = function (app) {
     app.get(API_PREFIX + '/version', function (request, response, next) {
-        response.send(1);
+        response.send(VERSION);
     });
 };
